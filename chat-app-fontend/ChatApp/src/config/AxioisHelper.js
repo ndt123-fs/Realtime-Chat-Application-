f03ec649f7772c128fef1bfaa5b91f65775ec6e6
Duy Tin <nduytin13112005@gmail.com>
2026-04-01 00:43:34 +0700

    fix: use environment variables for API URL

--- chat-app-fontend/ChatApp/src/config/AxioisHelper.js
+++ chat-app-fontend/ChatApp/src/config/AxioisHelper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:8080/";
+// export const baseUrl = "http://localhost:8080/";
+export const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:8080/";
 
 export const httpClient = axios.create({
     baseURL: baseUrl,
